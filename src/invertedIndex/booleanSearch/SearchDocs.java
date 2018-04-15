0fc0611f2a8abefdb6df161648782714f38de493
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2018-04-15 20:51:57 -0300

    Create constructor

--- src/invertedIndex/booleanSearch/SearchDocs.java
+++ src/invertedIndex/booleanSearch/SearchDocs.java
@@ -5,4 +5,12 @@ import java.util.LinkedList;
 
 public class SearchDocs {
     private Hashtable<String, LinkedList<Integer>> indexs;
+
+    public SearchDocs(String dataPath) {
+
+    }
+
+    private void createIndex() {
+        
+    }
 }
