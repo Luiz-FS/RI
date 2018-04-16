b0d7cec23454982c818c71b16fc5d9494dd53814
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2018-04-15 22:21:11 -0300

    Create Main class

--- src/invertedIndex/booleanSearch/SearchDocs.java
+++ src/invertedIndex/booleanSearch/SearchDocs.java
@@ -1,16 +1,29 @@
 package invertedIndex.booleanSearch;
 
+import java.io.*;
 import java.util.Hashtable;
 import java.util.LinkedList;
 
 public class SearchDocs {
+    private String dataPath;
     private Hashtable<String, LinkedList<Integer>> indexs;
 
     public SearchDocs(String dataPath) {
-
+        this.dataPath = dataPath;
+        createIndex();
     }
 
     private void createIndex() {
-        
+        File data = new File(dataPath);
+
+        try {
+            BufferedReader read = new BufferedReader(new FileReader(data));
+            String x = read.readLine();
+            System.out.println(x);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import invertedIndex.booleanSearch.SearchDocs;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SearchDocs docs = new SearchDocs("data/noticias_estadao.csv");
+    }
+}
